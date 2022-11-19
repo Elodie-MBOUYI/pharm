aa38ebad7cd35f7e54b34d7c728c6a1b7b4f904d
Elod-cpu <oulendoeloise@gmail.com>
2022-11-19 23:16:48 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Pharms from './components/Pharms'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home/>
+      <Pharms/>
     </div>
   );
 }
---
+++ src/components/Pharms.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+// import cap from './images/cap.png'
+import './Pharms.css'
+
+
+export default class MultipleItems extends Component {
+    render() {
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
+      return (
+        <div className="pharms">
+          <h2>VOS PHARMACIES</h2>
+          <span className="liner"></span>
+          <p>Avec Médic'App, s'approvisionner en produits et services pharmaceutiques n'a jamais été aussi simple dans votre ville !</p>
+          <Slider {...settings}>
+            <div className="step">
+              <button href='./' >Pharmacie Centrale</button>
+            </div>
+            <div className="stepy" >
+            <button href='./' >Pharmacie Andréa</button>
+            </div>
+            <div className="step">
+            <button href='./' >Pharmacie Banco</button>
+            </div>
+            <div className="stepy">
+            <button href='./' >Pharmacie RADOCA</button>
+            </div>
+            <div className="step">
+            <button href='./' >Pharmacie Von OKWA</button>
+            </div>
+            <div className="stepy">
+            <button href='./' >Pharmacie Grand Village</button>
+            </div>
+            
+          </Slider>
+        </div>
+      );
+    }
+  }
